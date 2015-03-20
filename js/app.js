faa605f4d5402d0d791912c3e38942ba04bc63f6
René A. Morales <rene.morales.sanchez@gmail.com>
2015-03-20 17:08:52 -0300

    Initial Platanus commit

--- js/app.js
+++ js/app.js
@@ -5,10 +5,12 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'app.env'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    console.log('Platanus');
+
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
